3cffe77f6949c6aec5da0bb2ddfbd17e0df832c6
Krish583 <86866463+Krish583@users.noreply.github.com>
2021-08-14 13:13:04 +0530

    Updated file with complete assignment

--- mrf.js
+++ mrf.js
@@ -1,36 +1,41 @@
 let url = 'https://restcountries.eu/rest/v2/all';
-console.log("Hello World!");
 
 fetch(url)
 .then(res => res.json())
 .then((out) => {
- const asia=out.filter((n) => n.region == "Asia").map((nm)=>nm.name);
- console.log("Asia countries are: ", asia);
- const count=out.filter((n) => n.population < 200000).map((nm)=>nm.name);
- console.log("Population of countries less than 2 Lakhs are: ", count);
- const reducer = (accumulator, currentValue) => accumulator + currentValue;
- total = out.map((n)=>n.population).reduce(reducer);
- console.log(" Total Popualation in the world:", total)
- const cur=out.filter((n) => {
-     for(let i=0;i<n.currencies.length;i++)
+
+// a. Get all the countries from Asia continent /region using Filter function
+const asia=out.filter(({region}) => region == "Asia").map(({name}) => name);
+console.log("Asia countries are: ", asia);
+
+//b. Get all the countries with a population of less than 2 lakhs using Filter function
+const count=out.filter(({population}) => population < 200000).map(({name}) => name);
+console.log("Population of countries less than 2 Lakhs are: ", count);
+
+//c. Print the following details name, capital, flag using forEach function
+console.log("Country_Name - Captial  - Flag")
+Object.entries(out).forEach((entry) => {
+    let name=entry[1].name;
+    let flag=entry[1].flag;a
+    let capital = entry[1].capital;
+  console.log(name +" - "+capital+" - "+flag);
+  });
+ 
+//d. Print the total population of countries using reduce function
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+total = out.map(({population})=>population).reduce(reducer);
+console.log("Total Popualation in the world:", total)
+
+//e. Print the country which uses US Dollars as currency
+const cur=out.filter(({currencies}) => {
+     for(let i=0;i<currencies.length;i++)
      {
-         if(n.currencies[i]['code'] == "USD"){
+         if(currencies[i]['code'] == "USD"){
              return true;
          }
      }  
- }).map((nm)=>nm.name);
- console.log("Countries with currency code USD are: ", cur);
-console.log(" Country Name  Captial  Flag")
- out.foreach(function(item){
-
-    console.log(item.name +" "+item.capital+" " + item.flag)
-    
-    })
+}).map(({name}) => name);
+console.log("Countries with currency code USD are: ", cur);
+ 
 })
 .catch(err => { throw err });
-
-// Print the following details name, capital, flag using forEach function
-
-
-
-
